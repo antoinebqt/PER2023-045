aa15fa4e67326fb4c43ae4d3418e50cfce8f8a1f
Antoine Buquet <antoine.buquet49@gmail.com>
2023-12-19 17:39:32 +0100

    partial fix install and deploy script

--- scripts/installRequirements.sh
+++ scripts/installRequirements.sh
@@ -20,20 +20,20 @@ printf "\n\033[1;36m## Installing Minikube\033[0m\n"
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-minikube start
+sudo minikube start --force #--cpus=4 --memory=4g
 
 # HELM
-printf "\n#\033[1;36m# Installing Helm\033[0m\n"
+printf "\n\033[1;36m## Installing Helm\033[0m\n"
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
 # PACKAGE PYTHON
-# printf "\n\033[1;36m## Installing Python packages\033[0m\n"
+printf "\n\033[1;36m## Installing Python packages\033[0m\n"
 
-# sudo apt install python3-pip
-# sudo apt install python3-kubernetes
+sudo apt install python3-pip
+sudo apt install python3-kubernetes
 
 # ANSIBLE
 printf "\n\033[1;36m## Installing Ansible\033[0m\n"
