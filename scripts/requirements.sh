fe033c3263b628c0c7e344dfde5122bdbb3dd148
Antoine Buquet <antoine.buquet49@gmail.com>
2023-12-19 10:01:49 +0100

    log in requirements.sh

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,4 +1,5 @@
 # DOCKER
+echo "Installation de Docker"
 
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
@@ -9,23 +10,30 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
 # KUBECTL
+echo "Installation de Kubectl"
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
 
 # MINIKUB
+echo "Installation de Minikub"
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube start
 
 # HELM
+echo "Installation de Helm"
 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
 # ANSIBLE
+echo "Installation de Ansible"
 
 sudo apt update
 sudo apt install ansible
+ansible --version
+
