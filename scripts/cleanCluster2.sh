5945563f667c1d50225d324408bfe38be533e4f3
Antoine Buquet <61704871+antoinebqt@users.noreply.github.com>
2024-02-05 13:26:49 +0100

    Update cleanCluster2.sh

--- scripts/cleanCluster2.sh
+++ scripts/cleanCluster2.sh
@@ -1,3 +1,5 @@
+kubectl delete kafkatopic --all -n default
+kubectl delete kafka --all -n default
 kubectl delete all --all -n default
 kubectl delete configmap --all -n default
 kubectl delete secret --all -n default
@@ -6,7 +8,4 @@ kubectl delete pvc --all -n default
 kubectl delete serviceaccount --all -n default
 kubectl delete role --all -n default
 kubectl delete rolebinding --all -n default
-kubectl delete kafkatopics --all -n default
-kubectl delete kafka --all -n default
 kubectl delete crd --all -n default
-
