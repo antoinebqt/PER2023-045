3b9a25a57952521b743d21b4c35ab1484d7615ed
Ayoub <youbima17@gmail.com>
2024-02-12 10:54:51 +0100

    script to clear prometheus

---
+++ scripts/cleanPrometheus.sh
@@ -0,0 +1,6 @@
+kubectl delete -f ../kubernetes/prometheus-operator-deployment.yaml
+kubectl delete -f ../kubernetes/prometheus-additional.yaml
+kubectl delete -f ../kubernetes/prometheus-rules.yaml
+kubectl delete -f ../kubernetes/strimzi-pod-monitor.yaml
+kubectl delete -f ../kubernetes/prometheus.yaml
+kubectl delete -f ../kubernetes/grafana.yaml
