3eeff022892072d010d31eb9dcce9457839bb65d
Antoine Buquet <antoine.buquet49@gmail.com>
2024-02-19 10:55:05 +0100

    end fix script requirements.sh

--- newScripts/requirements.sh
+++ newScripts/requirements.sh
@@ -1,15 +1,16 @@
 # PACKAGE PYTHON
 printf "\n\033[1;36m## Installing Python packages\033[0m\n"
+sudo apt update
 sudo apt install -y python3-pip
 sudo apt install -y python3-kubernetes
 
 # ANSIBLE
 printf "\n\033[1;36m## Installing Ansible\033[0m\n"
-sudo apt update
 sudo apt install -y ansible
 
 # KUBECTL
 printf "\n\033[1;36m## Installing Kubectl\033[0m\n"
+sudo apt install -y curl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
