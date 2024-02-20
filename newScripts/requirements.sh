174d8caf13c13e8d570bbf4873ca84ceafd1a70e
Antoine Buquet <61704871+antoinebqt@users.noreply.github.com>
2024-02-20 16:56:20 +0100

    Update requirements.sh

--- newScripts/requirements.sh
+++ newScripts/requirements.sh
@@ -3,6 +3,7 @@ printf "\n\033[1;36m## Installing Python packages\033[0m\n"
 sudo apt update
 sudo apt install -y python3-pip
 sudo apt install -y python3-kubernetes
+sudo apt install -y python3-pandas
 
 # ANSIBLE
 printf "\n\033[1;36m## Installing Ansible\033[0m\n"
